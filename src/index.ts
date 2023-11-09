5491ae900255146fc989001837da273a2f7c9453
ana-paula-andreis <99158852ana@gmail.com>
2023-11-09 16:37:11 -0300

    primeira atualização

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import 'reflect-metadata';
+
+const app = express();
+app.use(express.json());
+app.use(function(req, res, next) {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
+ res.header('Access-Control-Allow-Methods', 'GET,POST');
+ next();
+});
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+
+import routes from './routes';
+app.use(routes); // depois da definição do parser via json.
+
